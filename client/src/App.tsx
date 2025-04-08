ca8119c0606f65b55ca1266195ae2abcf6fee328
Gus <gus@macbookpro.lan>
2025-04-07 20:53:31 -0600

    refactored login and sign up forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,13 +11,37 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+// Construct main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an authorization header
+const authLink = setContext((_, { headers }) => {
+  // Get authentication token from local storage if it exists.
+  const token = localStorage.getItem('id_token');
+  // Return the headers to the context so httpLink can read them.
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
