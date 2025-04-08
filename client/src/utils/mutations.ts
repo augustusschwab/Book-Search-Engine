7b073a472b669bb7a45d29abf9ad809c21ee117a
Gus <gus@macbookpro.lan>
2025-04-07 19:17:46 -0600

    added client queries and mutation files.

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,17 +1,49 @@
 import { gql } from '@apollo/client';
 
-export const GET_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
-      }
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+        
     }
-  }
 `;
+
+export const ADD_USER = gql`
+    mutation addUser($input: UserInput!){
+        addUser(input: $input) {
+            user {
+                username
+                _id
+            }
+            token
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
+            bookId
+            title
+            authors
+            description
+            image
+            link
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            bookCount
+        }
+    }
+`
